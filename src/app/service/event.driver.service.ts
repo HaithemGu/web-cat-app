0fabe87c06ae79bcde6a489145bd323738951746
gueshaithem@gmail.com <gueshaithem@gmail.com>
2025-03-20 15:37:34 +0100

    subject event service

--- src/app/service/event.driver.service.ts
+++ src/app/service/event.driver.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { ActionEvent } from "./product.state";
 
-
+// c'est services pour gerer la communication entre les composant 
 @Injectable({providedIn:"root"})
 export class EventDriverService {
    sourceEventSubject:Subject<ActionEvent> = new Subject<ActionEvent>(); // creation de Subject
